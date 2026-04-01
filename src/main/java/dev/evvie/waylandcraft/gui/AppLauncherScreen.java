ccb2e6dc614a8071628632ac7abe5c2d5071e729
EVV1E <fabillo552@gmail.com>
2026-04-02 01:26:34 +0200

    Implement app launcher search functionality

--- src/main/java/dev/evvie/waylandcraft/gui/AppLauncherScreen.java
+++ src/main/java/dev/evvie/waylandcraft/gui/AppLauncherScreen.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.joml.Random;
-
 import dev.evvie.waylandcraft.WaylandCraft;
 import dev.evvie.waylandcraft.desktop.DesktopEntry;
 import dev.evvie.waylandcraft.render.RenderUtils;
@@ -15,6 +13,7 @@ import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.AbstractContainerWidget;
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.components.events.GuiEventListener;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.navigation.CommonInputs;
@@ -28,6 +27,7 @@ public class AppLauncherScreen extends Screen {
 	
 	private WaylandCraft wlc;
 	private AppList list;
+	private EditBox searchBox;
 	
 	public AppLauncherScreen(WaylandCraft wlc) {
 		super(Component.literal("App Launcher"));
@@ -42,14 +42,76 @@ public class AppLauncherScreen extends Screen {
 		list = new AppList(this, width / 2 - listWidth / 2, height / 2 - listHeight / 2, listWidth, listHeight);
 		this.addRenderableWidget(this.list);
 		
-		Random random = new Random();
-		List<DesktopEntry> entries = wlc.xdgManager.entries().stream().filter((e) -> e.visible && random.nextFloat() < 0.2f).toList();
+		// Search box is not added to widgets for custom focus / key enter rules
+		searchBox = new EditBox(font, width / 2 - listWidth / 2, list.getY() - 25, listWidth, 20, Component.literal("Search"));
+		searchBox.setResponder(this::filterSetEntries);
+		searchBox.setFocused(true); // Eternally focused
+		
+		List<DesktopEntry> entries = wlc.xdgManager.entries().stream().filter((e) -> e.visible).toList();
 		list.setEntries(entries);
+	}
+	
+	private int similarityScore(String hay, String needle) {
+		if(hay == null || needle == null) return 0;
+		hay = hay.toLowerCase();
+		needle = needle.toLowerCase();
+		
+		if(hay.equals(needle)) return 3;
+		if(hay.startsWith(needle)) return 2;
+		if(hay.contains(needle)) return 1;
+		return 0;
+	}
+	
+	private int sumSimilarityScore(String[] hays, String needle) {
+		int score = 0;
+		for(String hay : hays) {
+			score += similarityScore(hay, needle);
+		}
+		return score;
+	}
+	
+	private int entryMatchesStrScore(DesktopEntry entry, String str) {
+		if(!entry.visible) return -1;
+		int score = 0;
+		score += 3 * similarityScore(entry.name, str);
+		score += sumSimilarityScore(entry.keywords, str);
+		score += similarityScore(entry.comment, str);
+		score += similarityScore(entry.genericName, str);
+		score += similarityScore(entry.exec, str);
+		return score;
+	}
+	
+	private void filterSetEntries(String filter) {
+		List<DesktopEntry> entries = wlc.xdgManager.entries().stream()
+				.map((entry) -> new RankedDesktopEntry(entry, entryMatchesStrScore(entry, filter)))
+				.filter((r) -> r.score > 0)
+				.sorted((r1, r2) -> r2.score - r1.score)
+				.map((r) -> r.entry)
+				.toList();
+		list.setEntries(entries);
+	}
+	
+	@Override
+	public boolean keyPressed(int key, int scancode, int modifiers) {
+		if(searchBox.keyPressed(key, scancode, modifiers)) return true;
+		return super.keyPressed(key, scancode, modifiers);
+	}
+	
+	@Override
+	public boolean charTyped(char c, int i) {
+		if(searchBox.charTyped(c, i)) return true;
+		return super.charTyped(c, i);
 	}
 	
 	@Override
 	public boolean isPauseScreen() {
 		return false;
+	}
+	
+	@Override
+	public void render(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+		super.render(guiGraphics, mouseX, mouseY, partialTicks);
+		searchBox.render(guiGraphics, mouseX, mouseY, partialTicks);
 	}
 	
 	public void launch(DesktopEntry entry) {
@@ -292,5 +354,7 @@ public class AppLauncherScreen extends Screen {
 		}
 		
 	}
+	
+	private static record RankedDesktopEntry(DesktopEntry entry, int score) {}
 	
 }
